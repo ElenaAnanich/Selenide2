e0a58c6f8e646aa406aad147181be5d3b41eb51c
Lena k <ktyecz09@mail.ru>
2023-05-21 12:33:58 +0300

    add initial commit

--- src/test/java/DeliveryCard.java
+++ src/test/java/DeliveryCard.java
@@ -11,17 +11,20 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCard {
 
-    private String generate(int addDays,String pattern){
+    private String generate(int addDays, String pattern) {
+
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
-    public void DeliveryCardTest(){
+
+    public void DeliveryCardTest() {
         open("http://localhost:9999/");
         $("[data-test-id ='city'] input").setValue("Челябинск");
-        String currentDate =generate(3,"dd.MM.yyyy");
-        $("[data-test-id ='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.DELETE);
+        String currentDate = generate(3, "dd.MM.yyyy");
+        $("[data-test-id ='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id ='date'] input").sendKeys(currentDate);
-        $("[data-test-id ='name'] input").setValue("Петр Петрович");
+        $("[data-test-id ='name'] input").setValue("Петр Петрович Петров");
         $("[data-test-id ='phone'] input").setValue("+79521111111");
         $("[data-test-id ='agreement']").click();
         $("button.button").click();
